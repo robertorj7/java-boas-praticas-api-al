3445dae422c27fcf4edc8ab31f54ac9aa028dda2
Roberto Jr <rrjr87@outlook.com>
2025-03-06 17:28:00 -0300

    created AdocaoService

---
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -0,0 +1,18 @@
+package br.com.alura.adopet.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdocaoService {
+    public void solicitar() {
+
+    }
+
+    public void aprovar() {
+
+    }
+
+    public void reprovar() {
+
+    }
+}
