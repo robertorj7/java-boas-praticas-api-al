6e78e4a444f20a63d5409cfffaea59ee0e9fcfe2
Roberto Jr <rrjr87@outlook.com>
2025-03-20 17:14:23 -0300

    added Chain of Responsability pattern

--- src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamento.java
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamento.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-public class ValidacaoPetComAdocaoEmAndamento {
+public class ValidacaoPetComAdocaoEmAndamento implements ValidacaoSolicitacaoAdocao{
     @Autowired
     AdocaoRepository adocaoRepository;
 
