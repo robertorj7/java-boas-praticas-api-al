ed70f2707b9aef7a90461ec4b4048983c554e46c
Roberto Jr <rrjr87@outlook.com>
2025-03-23 16:23:07 -0300

    refactoring ValidacaoPetComAdocaoEmAndamento

--- src/main/java/br/com/alura/adopet/api/repository/PetRepository.java
+++ src/main/java/br/com/alura/adopet/api/repository/PetRepository.java
@@ -1,8 +1,10 @@
 package br.com.alura.adopet.api.repository;
 
 import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.StatusAdocao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
+    boolean existsByPetIdAndStatus(Long idPet, StatusAdocao statusAdocao);
 }
