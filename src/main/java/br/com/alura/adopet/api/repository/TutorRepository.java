2a3c9998cdc2c5f5dc3a2ca323f4073d6ff6b82e
Roberto Jr <rrjr87@outlook.com>
2025-03-23 16:27:14 -0300

    refactoring ValidacaoTutorComAdocaoEmAndamento

--- src/main/java/br/com/alura/adopet/api/repository/TutorRepository.java
+++ src/main/java/br/com/alura/adopet/api/repository/TutorRepository.java
@@ -1,6 +1,8 @@
 package br.com.alura.adopet.api.repository;
 
+import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.model.Tutor;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
@@ -9,4 +11,5 @@ public interface TutorRepository extends JpaRepository<Tutor, Long> {
 
     boolean existsByEmail(String email);
 
+    boolean existsByIdTutorAndStatus(@NotNull Long idTutor, StatusAdocao status);
 }
--- src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamento.java
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamento.java
@@ -2,14 +2,10 @@ package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
-import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 public class ValidacaoTutorComAdocaoEmAndamento implements ValidacaoSolicitacaoAdocao{
     @Autowired
@@ -19,13 +15,11 @@ public class ValidacaoTutorComAdocaoEmAndamento implements ValidacaoSolicitacaoA
     TutorRepository tutorRepository;
 
     public void validar(SolicitacaoAdocaoDto dto) {
-        Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
-        List<Adocao> adocoes = adocaoRepository.findAll();
+        boolean tutorTemAdocaoEmAndamento = tutorRepository.existsByIdTutorAndStatus(dto.idTutor(), StatusAdocao.AGUARDANDO_AVALIACAO);
 
-        for (Adocao a : adocoes) {
-            if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
-            }
+        if (tutorTemAdocaoEmAndamento) {
+            throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
         }
+
     }
 }
