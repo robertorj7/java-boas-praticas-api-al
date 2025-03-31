90b8dd101c3604fe4c64aa73b20011c27df70e4a
Roberto Jr <rrjr87@outlook.com>
2025-03-31 17:52:33 -0300

    refactoring Abrigo.java

--- src/main/java/br/com/alura/adopet/api/model/Abrigo.java
+++ src/main/java/br/com/alura/adopet/api/model/Abrigo.java
@@ -1,9 +1,7 @@
 package br.com.alura.adopet.api.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
@@ -15,25 +13,17 @@ public class Abrigo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @NotBlank
-    @Column(name = "nome")
     private String nome;
 
-    @NotBlank
     @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
-    @Column(name = "telefone")
     private String telefone;
 
-    @NotBlank
     @Email
-    @Column(name = "email")
     private String email;
 
     @OneToMany(mappedBy = "abrigo", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JsonManagedReference("abrigo_pets")
     private List<Pet> pets;
 
     @Override
