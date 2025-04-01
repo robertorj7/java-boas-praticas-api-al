35955365f57fd14081d312566ce75319aadd96dc
Roberto Jr <rrjr87@outlook.com>
2025-04-01 12:48:52 -0300

    created AbrigoService

---
+++ src/main/java/br/com/alura/adopet/api/service/AbrigoService.java
@@ -0,0 +1,42 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.AbrigoDto;
+import br.com.alura.adopet.api.dto.CadastrarAbrigoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.repository.AbrigoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AbrigoService {
+
+    @Autowired
+    private AbrigoRepository abrigoRepository;
+
+    @Autowired
+    private PetRepository petRepository;
+
+    public List<AbrigoDto> listar() {
+        return abrigoRepository
+                .findAll()
+                .stream()
+                .map(AbrigoDto::new)
+                .toList();
+    }
+
+    public void cadastrar(CadastrarAbrigoDTO dto) {
+        Boolean jaCadastrado = abrigoRepository.existsByNomeOrTelefoneOrEmail(dto.nome(), dto.telefone(), dto.email());
+
+        if (jaCadastrado) {
+            throw new ValidacaoException("Dados já cadastrados para outro abrigo!");
+        }
+
+        abrigoRepository.save(new Abrigo(dto));
+    }
+
+
+}
