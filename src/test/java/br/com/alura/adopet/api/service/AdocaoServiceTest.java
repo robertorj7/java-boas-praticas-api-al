c0ebf10d2ba1758ea0f68ea36f452f2f729343ef
robertorj7 <robertorj7@yahoo.com>
2025-09-25 11:09:45 -0300

    add AdocaoControllerTest

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -9,7 +9,6 @@ import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
@@ -19,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
