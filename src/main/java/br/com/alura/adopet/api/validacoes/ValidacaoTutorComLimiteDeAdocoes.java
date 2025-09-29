ef77c896478f8e6be18750d08df011c76ec03bd9
robertorj7 <robertorj7@yahoo.com>
2025-09-29 12:10:42 -0300

    add ValidacaoTutorComLimiteDeAdocoesTest

--- src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComLimiteDeAdocoes.java
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComLimiteDeAdocoes.java
@@ -3,22 +3,18 @@ package br.com.alura.adopet.api.validacoes;
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ValidacaoTutorComLimiteDeAdocoes implements ValidacaoSolicitacaoAdocao {
     @Autowired
-    TutorRepository tutorRepository;
+    AdocaoRepository repository;
 
     public void validar(SolicitacaoAdocaoDto dto) {
-        boolean tutorTemAdocaoAprovada = tutorRepository.existsByIdTutorAndStatus(dto.idTutor(), StatusAdocao.APROVADO);
-        int contador = 0;
+        Integer adocoesTutor = repository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO);
 
-        if (tutorTemAdocaoAprovada) {
-            contador = contador + 1;
-        }
-
-        if (contador == 5) {
+        if (adocoesTutor == 5) {
             throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
         }
     }
---
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComLimiteDeAdocoesTest.java
@@ -0,0 +1,43 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ValidacaoTutorComLimiteDeAdocoesTest {
+    @InjectMocks
+    private ValidacaoTutorComLimiteDeAdocoes validador;
+
+    @Mock
+    private AdocaoRepository repository;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+
+    @Test
+    void naoDeveriaPermitirSolicitacaoDeAdocaoTutorAtingiuLimiteDe5Adocoes() {
+        given(repository.countByTutorIdAndStatus(dto.idTutor(),StatusAdocao.APROVADO)).willReturn(5);
+
+        assertThrows(ValidacaoException.class,() ->validador.validar(dto));
+    }
+
+    @Test
+    void deveriaPermitirSolicitacaoDeAdocaoTutorNaoAtingiuLimiteDe5Adocoes() {
+        given(repository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO)).willReturn(4);
+
+        assertDoesNotThrow(() -> validador.validar(dto));
+    }
+}
